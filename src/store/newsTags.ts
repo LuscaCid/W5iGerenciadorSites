b8d99477c0a9f72c01c378d8d4ba8959b55ec23f
lucas cid <lucasfelipaaa@gmail.com>
2025-03-20 10:51:30 -0300

    feat: adiciona propriedade de title para filtrar noticias por esta forma

--- src/store/newsTags.ts
+++ src/store/newsTags.ts
@@ -2,9 +2,13 @@ import { create } from "zustand";
 import {Tag} from "../@types/Tag";
 interface NewsTagsContext {
     selectedTags : Array<Tag>;
+    title : string;
     setSelectedTags : (tags : Array<Tag>) => void;
+    setTitle : (title : string) => void;
 }
 export const useNewsTagsContext = create<NewsTagsContext>((set) => ({
     selectedTags : [],
+    title : "",
+    setTitle : (newTitle) => set({title : newTitle}),
     setSelectedTags : (tags) => set({selectedTags : tags}),
 }));
