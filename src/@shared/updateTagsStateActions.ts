225b0ade67cc84dd46d1002057e4fe52dedcf336
lucas cid <lucasfelipaaa@gmail.com>
2025-03-21 11:14:24 -0300

    refactor: funções de alteração de estado de tags apos requisoições realocadas em outro hook

---
+++ src/@shared/updateTagsStateActions.ts
@@ -0,0 +1,44 @@
+import {QueryClient} from "@tanstack/react-query";
+import {Noticia} from "../@types/News";
+import {Tag} from "../@types/Tag";
+import {CreateTagDto} from "../hooks/useTags.ts";
+
+interface Props {
+    queryClient : QueryClient
+}
+export const updateTagsStateActions = ({ queryClient } : Props) => {
+
+    function updateTagsInNewsState (tag : Tag, variables: CreateTagDto)
+    {
+        queryClient.setQueryData(["news"], (prev : { news : Noticia[]}) => (
+            prev.news.map((newsPrev) => ({
+                ...newsPrev,
+                tags : newsPrev.tags ? newsPrev.tags.map(prevTag => {
+                    if (prevTag.id_tag == tag.id_tag)
+                    {
+                        return { ...prevTag, nm_slug : variables.nm_slug } as Tag
+                    }
+                    return tag;
+                }) : [],
+            } as Noticia))
+        ))
+    }
+    function updateTagsState (tag : Tag, variables: CreateTagDto)
+    {
+        queryClient.setQueryData(["tags"],
+            (prev : Tag[]) => prev.map(prevTag => {
+                if (prevTag.id_tag == tag.id_tag) {
+                    return {
+                        ...tag,
+                        nm_slug : variables.nm_slug,
+                    } as Tag;
+                }
+                return prevTag;
+            }));
+
+    }
+    return {
+        updateTagsInNewsState,
+        updateTagsState
+    }
+}
