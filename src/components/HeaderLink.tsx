c80a80930391f1947881574f00001b816094b895
lucas cid <lucasfelipaaa@gmail.com>
2025-03-24 14:26:47 -0300

    feat: adiciona tooltip para links

--- src/components/HeaderLink.tsx
+++ src/components/HeaderLink.tsx
@@ -1,5 +1,7 @@
 import { Link, useLocation } from "react-router-dom";
 import { twMerge } from "tailwind-merge";
+import {HTMLAttributeAnchorTarget} from "react";
+import {Tooltip} from "@mui/material";
 
 interface HeaderLinkProps 
 {
@@ -7,21 +9,30 @@ interface HeaderLinkProps
     title : string;
     className? : string;
     onClick : () => void;
-    target? : React.HTMLAttributeAnchorTarget
+    split? : boolean;
+    target? : HTMLAttributeAnchorTarget
+    description? : string
 }
-export function HeaderLink ({ title, to, className, onClick, target = "_self" } : HeaderLinkProps) 
+export function HeaderLink ({ title, to, className, onClick, target = "_self" , split = true, description } : HeaderLinkProps)
 {
     const path = useLocation();
     const pathDictionary = path.pathname.split('/')[1];
-    const route = to.split('/')[1];
+    const route = split ? to.split('/')[1] : to;
     return (
-        <Link 
-            target={target}
-            onClick={onClick}
-            className={twMerge([`${pathDictionary == route ? "border-b-[4px] border-blue-500" : "border-b-[4px] border-transparent" } px-10 py-8 text-lg hover:bg-blue-500 hover:text-zinc-100 flex items-center justify-center transition duration-150 `], [className]) } 
-            to={to}
+        <Tooltip
+            enterDelay={300}
+            enterNextDelay={300}
+            title={description}
         >
-            {title}
-        </Link > 
+            <Link
+                target={target}
+                onClick={onClick}
+                className={twMerge([`${pathDictionary == route ? "border-b-[4px] border-blue-500" : "border-b-[4px] border-transparent" } px-10 py-8 text-lg hover:bg-blue-500 hover:text-zinc-100 flex items-center justify-center transition duration-150 `], [className]) }
+                to={to}
+            >
+                {title}
+            </Link >
+        </Tooltip>
+
     );
 }
