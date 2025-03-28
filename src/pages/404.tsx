d35acfe35f42bf4eb0e5be8e405f0117442a3699
lucas cid <lucasfelipaaa@gmail.com>
2025-03-28 17:43:20 -0300

    style: torna página 404 moldada para modo contraste

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,10 +3,10 @@ import { AlertCircle } from "lucide-react";
 export function NotFound()
 {
     return (
-        <main className="w-full h-full flex items-center justify-center">
-      <span className="2xl:text-6xl mt-20 lg:text-4xl md:text-2xl p-2 2x1:p-10 rounded-lg bg-zinc-300 font-bold flex items-center gap-2 select-none">
-        Página não encontrada, erro 404 <AlertCircle size={60}/>
-      </span>
-        </main>
+    <main className="w-full h-full flex items-center justify-center">
+          <span className="2xl:text-6xl mt-20 lg:text-4xl md:text-2xl p-2 2x1:p-10 rounded-lg bg-zinc-300 dark:bg-zinc-800 font-bold flex items-center gap-2 select-none">
+                Página não encontrada, erro 404 <AlertCircle size={60}/>
+          </span>
+    </main>
     )
 }
