35db964a3a37a0c613fe18ffca0cbac5eef374d7
lucas cid <lucasfelipaaa@gmail.com>
2025-04-10 17:54:34 -0300

    style: muda alguns estilos de overlay

--- src/components/Dialogs/CustomOverlay.tsx
+++ src/components/Dialogs/CustomOverlay.tsx
@@ -1,4 +1,4 @@
 import {Overlay} from "@radix-ui/react-dialog";
 
 export const CustomOverlay = () =>
-    <Overlay className={"z-50 fixed inset-0 w-screen bg-zinc-900/30 backdrop-blur-md"} />
+    <Overlay className={"z-[100] fixed inset-0 w-screen bg-zinc-900/30 backdrop-blur-md"} />
