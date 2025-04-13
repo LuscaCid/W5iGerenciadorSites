39915b1e76c21c17da51630ad7c6e7ad684b56f0
Lucas Cid <lucasfelipaaa@gmail.com>
2025-04-13 17:06:39 -0300

    style: melhoera skeletons

--- src/components/HomeSkeleton.tsx
+++ src/components/HomeSkeleton.tsx
@@ -7,7 +7,7 @@ export const HomeSkeleton = () => {
             <Skeleton
                 variant={"text"}
                 animation={"wave"}
-                className={"w-[150px] h-10"}
+                className={"w-[150px] h-10 dark:bg-zinc-800"}
             />
             <Skeleton
                 className="w-full min-h-[250px] dark:bg-zinc-800  rounded-2xl"
@@ -17,7 +17,7 @@ export const HomeSkeleton = () => {
             <Skeleton
                 variant={"text"}
                 animation={"wave"}
-                className={"w-[150px] h-10"}
+                className={"w-[150px] h-10 dark:bg-zinc-800"}
             />
             <Skeleton
                 className="w-full min-h-[250px] dark:bg-zinc-800  rounded-2xl"
@@ -27,7 +27,7 @@ export const HomeSkeleton = () => {
             <Skeleton
                 variant={"text"}
                 animation={"wave"}
-                className={"w-[150px] h-10"}
+                className={"w-[150px] h-10 dark:bg-zinc-800"}
             />
             <header className={"flex-col md:flex-row flex gap-10 items-start"}>
                 <Skeleton
@@ -51,7 +51,7 @@ export const HomeSkeleton = () => {
                     }
                 </aside>
             </header>
-            <Skeleton variant={"text"} className={"lg:w-1/4 w-full"}/>
+            <Skeleton variant={"text"} className={"lg:w-1/4 w-full dark:bg-zinc-800"}/>
             <main className={"grid grid-cols-1 md:grid-cols-3 gap-4 w-2/2"}>
                 {Array.from({length : 6}).map((_, i) => (
                     <NewsCardSkeleton width={400} key={i} />
--- src/components/NewsCardSkeleton.tsx
+++ src/components/NewsCardSkeleton.tsx
@@ -6,21 +6,21 @@ export const NewsCardSkeleton = ({ width = 329 } : Props) => {
     return (
         <div className={`flex flex-col relative rounded-t-2xl  rounded-b-2xl shadow-lg max-h-[380px] w-full h-full max-w-[${width}px]`}>
             <Skeleton
-                className={"rounded-t-2xl"}
+                className={"rounded-t-2xl dark:bg-zinc-800"}
                 variant="rectangular"
                 animation={"pulse"}
                  height={185}
             />
             <Skeleton
-                className={"rounded-b-2xl"}
+                className={"rounded-b-2xl dark:bg-zinc-800"}
                 animation={"wave"}
                 height={192}
                 variant="rectangular"
             />
-            <Skeleton variant={"text"} className={"absolute border-none bottom-24 right-4 left-4 "}/>
-            <Skeleton variant={"text"} className={"absolute border-none bottom-20 right-4 left-4 "}/>
-            <Skeleton variant={"text"} className={"absolute border-none bottom-16 right-4 left-4"}/>
-            <Skeleton variant={"text"} className={"absolute border-none bottom-32 right-4 left-4 p-5"}/>
+            <Skeleton variant={"text"} className={"dark:bg-zinc-700 absolute border-none bottom-24 right-4 left-4 "}/>
+            <Skeleton variant={"text"} className={"dark:bg-zinc-700 absolute border-none bottom-20 right-4 left-4 "}/>
+            <Skeleton variant={"text"} className={"dark:bg-zinc-700 absolute border-none bottom-16 right-4 left-4"}/>
+            <Skeleton variant={"text"} className={"dark:bg-zinc-700 absolute border-none bottom-32 right-4 left-4 p-5"}/>
         </div>
 
 
